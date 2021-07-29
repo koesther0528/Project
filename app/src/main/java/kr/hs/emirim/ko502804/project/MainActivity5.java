9333721c127acc2ffc83cd503ff9e78608802fe8
esthe <ko990727@hmail.com>
2021-07-29 14:47:00 +0900

    activity_main5.xml

---
+++ app/src/main/java/kr/hs/emirim/ko502804/project/MainActivity5.java
@@ -0,0 +1,14 @@
+package kr.hs.emirim.ko502804.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main5);
+    }
+}
