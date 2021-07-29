0191a2dab19b96c0b746829e8e873df617c2b391
esthe <ko990727@hmail.com>
2021-07-29 16:06:29 +0900

    Commit

--- app/src/main/java/kr/hs/emirim/ko502804/project/MainActivity2.java
+++ app/src/main/java/kr/hs/emirim/ko502804/project/MainActivity2.java
@@ -22,5 +22,41 @@ public class MainActivity2 extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        Button main2_btn2 = findViewById(R.id.main2_btn2);
+        main2_btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity2.this,MainActivity4.class);
+                startActivity(intent);
+            }
+        });
+
+        Button main2_btn3 = findViewById(R.id.main2_btn3);
+        main2_btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity2.this,MainActivity5.class);
+                startActivity(intent);
+            }
+        });
+
+        Button main2_btn4 = findViewById(R.id.main2_btn4);
+        main2_btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity2.this,MainActivity6.class);
+                startActivity(intent);
+            }
+        });
+
+        Button main2_btn5 = findViewById(R.id.main2_btn5);
+        main2_btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity2.this,MainActivity7.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/kr/hs/emirim/ko502804/project/MainActivity7.java
@@ -0,0 +1,14 @@
+package kr.hs.emirim.ko502804.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity7 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main7);
+    }
+}
