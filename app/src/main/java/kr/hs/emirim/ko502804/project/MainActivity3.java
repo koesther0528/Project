f4c945efa4345b50e6ad39adc6db47dae9b247b7
esthe <ko990727@hmail.com>
2021-08-26 20:39:19 +0900

    Commit

--- app/src/main/java/kr/hs/emirim/ko502804/project/MainActivity3.java
+++ app/src/main/java/kr/hs/emirim/ko502804/project/MainActivity3.java
@@ -2,7 +2,10 @@ package kr.hs.emirim.ko502804.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity3 extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        Button result_main3 = findViewById(R.id.result_main3);
+        result_main3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity3.this, result.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/kr/hs/emirim/ko502804/project/MainActivity4.java
+++ app/src/main/java/kr/hs/emirim/ko502804/project/MainActivity4.java
@@ -2,7 +2,10 @@ package kr.hs.emirim.ko502804.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity4 extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity4 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
+
+        Button result_main4 = findViewById(R.id.result_main4);
+        result_main4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity4.this, result2.class);
+                startActivity(intent);
+            }
+        });
     }
 }
